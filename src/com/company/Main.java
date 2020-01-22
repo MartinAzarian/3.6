d448edd88dabccafdd4af5a1557d27e7b6901120
MartsLine <48711841+MartsLine@users.noreply.github.com>
2020-01-23 00:22:34 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        arrayList.add(2);
+        arrayList.add(4);
+        arrayList.add(3);
+        arrayList.add(6);
+        arrayList.add(7);
+
+       arrayList.remove(2);
+
+    }
+}
